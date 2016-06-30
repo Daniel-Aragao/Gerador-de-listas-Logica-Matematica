57056813b7941b9d7814df5f7cb084b0de0b00b0
Daniel-Aragao <dan.dan.44@hotmail.com>
2016-06-30 00:21:45 -0300

    ctrl shift f

--- src/ImportFormulas.java
+++ src/ImportFormulas.java
@@ -8,24 +8,19 @@ import java.util.ArrayList;
 public class ImportFormulas {
 	private static int nQuestoes;
 
-	public static ArrayList<String> getAll(){
+	public static ArrayList<String> getAll() {
 		ArrayList<String> questoes = new ArrayList<String>();
 		nQuestoes = 0;
-
 
 		try {
 			File fileDir = new File("res/Exercicios.txt");
 
-			BufferedReader buffer = new BufferedReader(
-			   new InputStreamReader(
-	                      new FileInputStream(fileDir), "Unicode"));
-
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(new FileInputStream(fileDir), "Unicode"));
 
 			String line = "";
-			while((line = buffer.readLine())!= null){
+			while ((line = buffer.readLine()) != null) {
 				questoes.add(line);
 			}
-
 
 			buffer.close();
 		} catch (IOException e) {
@@ -37,22 +32,19 @@ public class ImportFormulas {
 		return questoes;
 	}
 
-	public static int getNQuestoes(){
+	public static int getNQuestoes() {
 		return nQuestoes;
 	}
 
-	public static String getTextFrom(String path){
+	public static String getTextFrom(String path) {
 		String texto = "";
-
 
 		try {
 			File fileDir = new File(path);
 
-			BufferedReader buffer = new BufferedReader(
-			   new InputStreamReader(
-			              new FileInputStream(fileDir), "Unicode"));
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(new FileInputStream(fileDir), "Unicode"));
 			String line = "";
-			while((line = buffer.readLine()) != null){
+			while ((line = buffer.readLine()) != null) {
 				texto += line + "%";
 			}
 			buffer.close();
@@ -60,7 +52,6 @@ public class ImportFormulas {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 
 		return texto;
 	}
